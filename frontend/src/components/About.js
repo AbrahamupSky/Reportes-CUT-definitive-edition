57ccf38747580caa413a23d73c2398dc2d0e7304
AbrahamupSky <abrahamup7@gmail.com>
2022-10-25 12:46:01 -0500

    About añadido y terminado

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,6 +7,7 @@ export const About = (props) => {
     return (
         <div className="container mt-5 text-center">
             <h1>Coco{props.value}  </h1>
+            <h2>No pues, es la pagina shida en la que los maestros pueden subir archivos, y donde pueden quemar a sus alumnos haha k creisi</h2>
             
         </div>
     )
