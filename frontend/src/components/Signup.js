73385ff34dc511a7a657ef74a8c6399328335cc2
AbrahamupSky <abrahamup7@gmail.com>
2022-10-28 18:10:09 -0500

    Update Signup.js
    About

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -111,7 +111,8 @@ export const Signup = () => {
                                 id="inputPassword" required />
                         </div>
                         }
-                        {rol1 === "1" || rol1 === "7" && !form1Active? setForm1Active(!form1Active):
+
+                        {rol1 === "1" && !form1Active? setForm1Active(!form1Active):
                         form1Active && <div className="col-md-6">
                             <label htmlFor="exampleSelect1" className="form-label mt-4">Especifica el rol</label>
                             <select onChangeCapture={selectArea} defaultValue='0' className="form-select" id="exampleSelect1" onClick={e => setRol(e.target.value)} required>
@@ -122,6 +123,7 @@ export const Signup = () => {
                             </select>
                         </div>
                         }
+                        
                         {rol1 === "3" && !form2Active? setForm2Active(!form2Active):
                         form2Active && <div className="col-md-6">
                             <label htmlFor="exampleSelect3" className="form-label mt-4">Especifica el rol</label>
